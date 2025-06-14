9050a8e31bf3350a91acf2f7540a183f9bf597cf
jasjotp <jasjotp@sfu.ca>
2025-06-14 12:25:14 -0700

    created shutdown function

--- producer/main.py
+++ producer/main.py
@@ -1,8 +1,10 @@
 import os
 from confluent_kafka import Producer
 import logging 
+import random
 from dotenv import load_dotenv
 from faker import Faker
+
 
 logging.basicConfig(
     format = '%(asctime)s - %(levelname)s - %(module)s - %(message)s',
@@ -53,7 +55,33 @@ class TransactionProducer():
             raise e 
         
         # handle the compromised users/merchants, that we know are risky
-        
+        self.compromised_users = set(random.sample(range(1000, 9999), 50)) # set 0.5% of users to be comprimised users (0.5% so the model does not predict comprimised users too often)
+        self.high_risk_merchants = ['QuickCash', 'GlobalDigital', 'FastMoneyX']
+        self.fraud_pattern_weights = {
+            'account_takeover': 0.4, # 40% of fraud cases when someone takes over your account (they get your password for example, as this is the most common for fraud cases)
+            'card_testing': 0.3, # 30% of fraud cases when someone tests your card to see if it has money or not
+            'merchant_collusion': 0.2, # 20% 
+            'geo_anomaly': 0.1 # 10% of fraud cases when someone uses your card from a different location
+        }
+
+        # configure graceful shutdown 
+        signal.signal(signal.SIGINT, self.shutdown)
+        signal.signal(signal.SIGTERM, self.shutdown)
+
+    # create a function to generate transactions 
+    def run_continuous_production(self, interval: float = 0.0):
+        pass:
+
+    # create the shutdown function 
+    def shutdown(self, signum = None, frame = None):
+        if self.running:
+            logger.info('Initializing shutdown...')
+            self.running = False
+
+            if self.producer:
+                self.producer.flush(timeout = 30)
+                self.producer.close()
+            logger.info('Producer stopped...')
 
 # entry point
 if __name__ == "__main__":
