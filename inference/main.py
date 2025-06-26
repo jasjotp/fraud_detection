913469076e3ef8d3142ff1f268f43cb158f5043a
jasjotp <jasjotp@sfu.ca>
2025-06-26 11:13:52 -0700

    added configuration details and json schema for inference

--- inference/main.py
+++ inference/main.py
@@ -2,8 +2,13 @@ import os
 import logging
 import pickle 
 from pyspark.sql import SparkSession
+from pyspark.sql.types import (StructType, StructField, StringType,
+                              IntegerType, DoubleType, TimestampType)
 from dotenv import load_dotenv
 import yaml
+import json
+
+from dags.fraud_detection_training.py import read_from_kafka
 
 logging.basicConfig(
     level = logging.INFO,
@@ -66,6 +71,60 @@ class FraudDetectionInference:
         except Exception as e:
             logger.error(f'Error initiailizing Spark Session: {e}')
             raise
+    
+    # helper function to read data from Kafka
+    def read_from_kafka(self):
+        # load Kafka configuration parameters with fallback values
+        logger.info(f'Reading data from Kafka topic: {self.config['kafka']['topic']}')
+        kafka_bootstrap_servers = self.config['kafka']['bootstrap_servers']
+        kafka_topic = self.config['kafka']['topic']
+        kafka_security_protocol = self.config['kafka'].get('security_protocol', 'SASL_SSL')
+        kafka_sasl_mechanism = self.config['kafka'].get('sasl_mechanism', 'PLAIN')
+        kafka_username = self.config['kafka'].get('username')
+        kafka_password = self.config['kafka'].get('password')
+
+        # configure Kafka SASL authentication string
+        kafka_sasl_jaas_config = (
+            f'org.apache.kafka.common.security.plain.PlainLoginModule required '
+            f'username="{kafka_username}" password="{kafka_password}";'
+        )
+
+        # store configuration details in instannce variables for reuse 
+        self.bootstrap_servers = kafka_bootstrap_servers
+        self.topic = kafka_topic
+        self.security_protocol = kafka_security_protocol
+        self.sasl_mechanism = kafka_sasl_mechanism
+        self.username = kafka_username
+        self.password = kafka_password
+        self.sasl_jaas_config = kafka_sasl_jaas_config
+        
+        # use readStream to read from Kafka
+        df = (self.spark.readStream
+              .format('kafka') 
+              .option('kafka.bootstrap.servers', kafka_bootstrap_servers) 
+              .option('subscribe', kafka_topic) 
+              .option('startingOffsets', 'latest') 
+              .option('kafka.security.protocol', kafka_security_protocol) 
+              .option('kafka.sasl.mechanism', kafka_sasl_mechanism)  
+              .option('kafka.sasl.jaas.config', kafka_sasl_jaas_config)
+              .load()
+            )
+
+        # define the JSON schema for the incoming transactions 
+        json_schema = StructType([
+            StructField('transaction_id', StringType(), nullable = True),
+            StructField('user_id', IntegerType(), nullable = True),
+            StructField('amount', DoubleType(), nullable = True),
+            StructField('currency', StringType(), nullable = True),
+            StructField('merchant', StringType(), nullable = True),
+            StructField('timestamp', TimestampType(), nullable = True),
+            StructField('location', StringType(), nullable = True)
+        ])
+        
+    # function to run infernence on the model so it is trained on new data that comes in 
+    def run_inference(self):
+        df = self.read_from_kafka()
+        df = self.add_features(df)
 
 if __name__ == "__main__":
     inference = FraudDetectionInference(config_path = '/app/config.yaml')
